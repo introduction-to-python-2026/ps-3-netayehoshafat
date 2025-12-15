68f019871e43d3e524ce32a5d11181c82881dbd3
netayehoshafat <nettaye@gmail.com>
2025-12-15 14:31:10 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,8 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+    if n_terms == 0:
+        return 0
+    Leib_sum = 0
+    for n in range(n_terms):
+        Leib_sum += ((-1) ** n) / ((2 * n) + 1)
+    approximate_pi = 4.0 * Leib_sum
+    return approximate_pi
